45590e1d71fc56834aa25c37ce67b2a2ad90a379
Sumit Gupta <69911115+Sumit79-coder@users.noreply.github.com>
2022-08-10 15:51:33 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req, res){
+
+
+var num1= req.body.num1;
+var num2= req.body.num2;
+var result = num1 + num2;
+
+console.log(req.body);
+res.send("Result is "+ result)
+} );
+
+app.listen(3000,function(){
+    console.log("Server is at port 3000")
+});
